3621b5d3f621a7b850c2caa13e4ab2aebe24dea3
Delibero69 <deliberin@gmail.com>
2023-05-02 00:07:30 +0300

    first commit

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+
+
+from django.urls import path, include
+from users.views import *
+
+app_name = 'users'
+
+
+urlpatterns = [
+    path('login/', login,name='login'),
+    path('registration/', registration,name='registration'),
+    path('profile/', profile,name='profile'),
+    path('logout/', logout,name='logout'),
+
+]
